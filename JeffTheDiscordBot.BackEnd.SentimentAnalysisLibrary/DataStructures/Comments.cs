70818c0af89d16f9cd7bf1f1097dafdec3abb8c7
ZieMcd <59741700+ZieMcd@users.noreply.github.com>
2022-03-16 20:41:05 +0200

    starting coding the Ml library

---
+++ JeffTheDiscordBot.BackEnd.SentimentAnalysisLibrary/DataStructures/Comments.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.ML.Data;
+
+namespace JeffTheDiscordBot.BackEnd.SentimentAnalysisLibrary.DataStructures;
+
+public class Comments
+{
+    [LoadColumn(0)]
+    public string Comment { get; set; }
+    
+    [LoadColumn(1)]
+    public bool Label { get; set; }
+    
+}
