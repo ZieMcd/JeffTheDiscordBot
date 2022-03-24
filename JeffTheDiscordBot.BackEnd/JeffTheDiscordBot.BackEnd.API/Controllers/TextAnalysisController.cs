d472a708d896359ccc40fbbaafa13de1ddeb3992
ZieMcd <59741700+ZieMcd@users.noreply.github.com>
2022-03-24 21:07:29 +0200

    Added Controller

---
+++ JeffTheDiscordBot.BackEnd/JeffTheDiscordBot.BackEnd.API/Controllers/TextAnalysisController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace JeffTheDiscordBot.BackEnd.API.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class TextAnalysisController : Controller
+{
+    public IActionResult AnalyzeText(string text)
+    {
+        throw new NotImplementedException();
+    }
+}
